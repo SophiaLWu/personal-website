da66d3389007099da86977c058f8ccbf4d826450
Sophia Wu <sophia.luxi.wu@gmail.com>
2017-05-17 11:21:41 -0700

    Make changes to projects tab and body text

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   animateNavbar();
   animatePage();
 
-  var projectsSlider = new Slider(9, "projects");
+  var projectsSlider = new Slider(10, "projects");
   projectsSlider.init();
   projectsSlider.userInput();
   addPortfolioDescriptions()
@@ -75,16 +75,18 @@ $(document).ready(function() {
     $("div[data-slide-num='3']").find("img").wrap("<a href='https://sophialwu.github.io/minesweeper/'></a>");
     $("div[data-slide-num='4']").find(".project-description").append("<a href='https://sophialwu.github.io/missile-command/'>MISSILE COMMAND</a>: Atari's game built using HTML5 Canvas"); 
     $("div[data-slide-num='4']").find("img").wrap("<a href='https://sophialwu.github.io/missile-command/'></a>");
-    $("div[data-slide-num='5']").find(".project-description").append("<a href='https://sophialwu.github.io/snake-game/'>SNAKE</a>: JavaScript/jQuery web game");
-    $("div[data-slide-num='5']").find("img").wrap("<a href='https://sophialwu.github.io/snake-game/'></a>");
+    $("div[data-slide-num='5']").find(".project-description").append("<a href='https://infinite-fortress-13459.herokuapp.com/'>FLIGHTBOOKER</a>: Ruby on Rails flight searching and booking web app"); 
+    $("div[data-slide-num='5']").find("img").wrap("<a href='https://infinite-fortress-13459.herokuapp.com/'></a>");
     $("div[data-slide-num='6']").find(".project-description").append("<a href='https://quiet-forest-50554.herokuapp.com/'>PRIVATE EVENTS</a>: Ruby on Rails event planning web app utilizing Bootstrap");
     $("div[data-slide-num='6']").find("img").wrap("<a href='https://quiet-forest-50554.herokuapp.com/'></a>");
-    $("div[data-slide-num='7']").find(".project-description").append("<a href ='https://sophialwu.github.io/tic-tac-toe/'>TIC-TAC-TOE</a>: minimalist web game built with JavaScript/jQuery");
-    $("div[data-slide-num='7']").find("img").wrap("<a href='https://sophialwu.github.io/tic-tac-toe/'></a>");
-    $("div[data-slide-num='8']").find(".project-description").append("<a href='https://rawgit.com/SophiaLWu/project-javascript-jquery/master/index.html'>ETCH-A-SKETCH</a>: JavaScript/jQuery web app");
-    $("div[data-slide-num='8']").find("img").wrap("<a href='https://rawgit.com/SophiaLWu/project-javascript-jquery/master/index.html'></a>");
-    $("div[data-slide-num='9']").find(".project-description").append("<a href='https://github.com/SophiaLWu/zuma-game'>ZUMA</a>: Puzzle game built using Python's Tkinter GUI");
-    $("div[data-slide-num='9']").find("img").wrap("<a href='https://github.com/SophiaLWu/zuma-game'></a>");
+    $("div[data-slide-num='7']").find(".project-description").append("<a href='https://sophialwu.github.io/snake-game/'>SNAKE</a>: JavaScript/jQuery web game");
+    $("div[data-slide-num='7']").find("img").wrap("<a href='https://sophialwu.github.io/snake-game/'></a>");
+    $("div[data-slide-num='8']").find(".project-description").append("<a href ='https://sophialwu.github.io/tic-tac-toe/'>TIC-TAC-TOE</a>: minimalist web game built with JavaScript/jQuery");
+    $("div[data-slide-num='8']").find("img").wrap("<a href='https://sophialwu.github.io/tic-tac-toe/'></a>");
+    $("div[data-slide-num='9']").find(".project-description").append("<a href='https://rawgit.com/SophiaLWu/project-javascript-jquery/master/index.html'>ETCH-A-SKETCH</a>: JavaScript/jQuery web app");
+    $("div[data-slide-num='9']").find("img").wrap("<a href='https://rawgit.com/SophiaLWu/project-javascript-jquery/master/index.html'></a>");
+    $("div[data-slide-num='10']").find(".project-description").append("<a href='https://github.com/SophiaLWu/zuma-game'>ZUMA</a>: Puzzle game built using Python's Tkinter GUI");
+    $("div[data-slide-num='10']").find("img").wrap("<a href='https://github.com/SophiaLWu/zuma-game'></a>");
   }
 
 });
